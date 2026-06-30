4ac73178524a9109667ff53e3b23d69fd3d2057b
vinkin <vinkin@vinkinnoMacBook-Air.local>
2026-07-01 08:59:01 +0900

    左侧头部布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import './App.css'
-
+import { Moon, Search } from 'lucide-react';
+import './App.css';
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div className="container">
+    <aside className='sidebar'>
+      <div className="sidebar-header">
+        <div className="brand-block">
+          <div className="brand-mark">T</div>
+          <div>
+            <h1>Todo List</h1>
+            <span>Local JSON Workspace</span>
+          </div>
+        </div>
+        <button className='icon-button' type="button"><Moon size={18} /></button>
+      </div>
+    </aside>
+    </div>
   )
 }
 
